b2ea04191f656d7c2ad559de43eb345ed1bc92e7
Alejandro oviedo <https://github.com/startan78>
2022-10-20 22:07:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/optionmenuitemclick/Setting.java
@@ -0,0 +1,15 @@
+
+package com.example.optionmenuitemclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
